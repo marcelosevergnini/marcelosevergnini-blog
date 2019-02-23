34b568b5fe57884b746169342a3dd53ee3eccd77
marcelosevergnini <marcelosevergnini@gmail.com>
2019-02-23 15:34:21 +0000

    Alterado basic user configs

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 module.exports = {
-  title: 'Dev Blog', // Required
-  author: 'Ryan Fitzgerald', // Required
+  title: 'Art at Code', // Required
+  author: 'Marcelo Severgnini', // Required
   description: 'Full-stack Web Developer',
-  primaryColor: '#3498db', // Required
+  primaryColor: '#c13f3f', // Required
   showHeaderImage: true,
   showShareButtons: true,
   postsPerPage: 5, // Required
   social: {
-    website: 'https://ryanfitzgerald.ca',
-    github: 'https://github.com/ryanfitzgerald',
-    twitter: 'https://twitter.com/ryanafitzgerald',
-    linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald',
+    website: 'https://marcelo-severgnini-dev.netlify.com/',
+    github: 'https://github.com/marcelosevergnini',
+    twitter: 'https://twitter.com/marcelosevergni',
+    linkedin: 'https://www.linkedin.com/in/marcelosevergnini/',
   },
-  pathPrefix: '/devblog',
-  siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
+  pathPrefix: '/artcode',
+  siteUrl: 'https://github.com/marcelosevergnini/marcelosevergnini-blog/',
 };
