bdfdab39115db481e2416e07cf5576f192f65371
marcelosevergnini <marcelosevergnini@gmail.com>
2019-02-23 17:21:24 +0000

    Added Google analytics tracking-id

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,7 +46,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-112981926-2`,
       },
     },
     `gatsby-plugin-feed`,
